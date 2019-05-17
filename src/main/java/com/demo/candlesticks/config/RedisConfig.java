1fdcb19809830eb69373ccabf1a58364d1a3349c
Dmitriy Ovchinnikov <ovchinnikov.d94@gmail.com>
2019-05-17 19:44:23 +0300

    Lettuce connestion Factory

--- src/main/java/com/demo/candlesticks/config/RedisConfig.java
+++ src/main/java/com/demo/candlesticks/config/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -17,12 +18,11 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        return factory;
+        return new LettuceConnectionFactory();
     }
 
     @Bean
-    public RedisTemplate<String, Trade> redisTemplateTrade(){
+    public RedisTemplate<String, Trade> redisTemplate(){
         RedisTemplate<String, Trade> redisTemplate = new RedisTemplate<>();
         redisTemplate.setDefaultSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory());
